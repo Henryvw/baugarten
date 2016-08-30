7ef7c110ad06f668a80fde8348e3dde0283f4bda
Henryvw <Henryvw@gmail.com>
2016-08-30 17:48:24 +0200

    weird .gitignore change... made me re-save all git additions... not sure if this spells trouble

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
+app.listen(3001, function() {
   console.log('listening on 3000')
-  console.log(__dirname)
+  console.log(__dirname + '/public')
+ 
 })
 
 app.get('/', (request, response) => {
     console.log(__dirname)
     response.sendFile(__dirname + '/index.html')
 })
+
+app.use(express.static(__dirname + '/public'));
 
