5a6f914a8572b0fffc1982c8ee0afa9c5c9b19d2
Henry vW <henryvanwagenberg@henrys-air.fritz.box>
2018-01-29 14:16:56 +0100

    fixed console.log port and updated year to 2018

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3001, function() {
-  console.log('listening on 3000')
+  console.log('listening on 3001')
   console.log(__dirname + '/public')
  
 })
